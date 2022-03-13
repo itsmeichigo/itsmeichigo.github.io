300c4a82f1e9256128f92a8792c17da96316b419
Huong Do <hi.huongdo@gmail.com>
2022-03-13 22:59:00 +0700

    Update peachy page

--- static/peachy/js/scripts.js
+++ static/peachy/js/scripts.js
@@ -11,39 +11,21 @@ $(function () {
     $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 20
+            scrollTop: $($anchor.attr('href')).offset().top - 50
         }, 1000);
         event.preventDefault();
     });
 
-    // slick slider
-    $('.slick-about').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        dots: true,
-        arrows: false
-    });
-
     //toggle scroll menu
-    var scrollTop = 0;
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         //adjust menu background
-        if (scroll > 80) {
-            if (scroll > scrollTop) {
-                $('.smart-scroll').addClass('scrolling').removeClass('up');
-            } else {
-                $('.smart-scroll').addClass('up');
-            }
+        if (scroll >= 100) {
+            $('.sticky-navigation').addClass('navbar-shadow');
         } else {
-            // remove if scroll = scrollTop
-            $('.smart-scroll').removeClass('scrolling').removeClass('up');
+            $('.sticky-navigation').removeClass('navbar-shadow');
         }
-
-        scrollTop = scroll;
-
+        
         // adjust scroll to top
         if (scroll >= 600) {
             $('.scroll-top').addClass('active');
@@ -60,14 +42,13 @@ $(function () {
         }, 1000);
     });
 
-    /**Theme switcher - DEMO PURPOSE ONLY */
-    $('.switcher-trigger').click(function () {
-        $('.switcher-wrap').toggleClass('active');
-    });
-    $('.color-switcher ul li').click(function () {
-        var color = $(this).attr('data-color');
-        $('#theme-color').attr("href", "css/" + color + ".css");
-        $('.color-switcher ul li').removeClass('active');
-        $(this).addClass('active');
+    // slick slider
+    $('.slick-about').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false
     });
 });
